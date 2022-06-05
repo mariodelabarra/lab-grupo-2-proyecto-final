4971cedab5103e04161871f6b65b24c4a1f0bea6
matearg <mateoarcidiacono@gmail.com>
2022-06-04 23:11:58 -0300

    Codigo formateado

--- pedidos/main.c
+++ pedidos/main.c
@@ -1,26 +1,26 @@
+#include "articulos.h"
+#include "fecha.h"
+#include <conio.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <conio.h>
-#include "fecha.h"
-#include "articulos.h"
 
 typedef struct
 {
-    int idPedido;        // auto incremental
-    int idCliente;       // este valor corresponde al id del cliente que realizó el pedido
-    char fecha[12];      // dd/MM/aaaa
-    float costoTotal;    // sumatoria de costoPorUnidad
-    int pedidoAnulado;    // indica 1 o 0 si el pedido fue anulado o no
+    int idPedido;      // auto incremental
+    int idCliente;     // este valor corresponde al id del cliente que realizó el pedido
+    char fecha[12];    // dd/MM/aaaa
+    float costoTotal;  // sumatoria de costoPorUnidad
+    int pedidoAnulado; // indica 1 o 0 si el pedido fue anulado o no
 } stPedido;
 
 void altaPedido();
-// void bajaPedido();
-// void modificacionPedido();
-// void consultaPedido();
-// void listadoPedido();
+void bajaPedido();
+void modificacionPedido();
+void consultaPedido();
+void listadoPedido();
 
-int main()
+int main() // Temporal
 {
     altaPedido();
 }
@@ -28,25 +28,19 @@ int main()
 void altaPedido()
 {
     printf("Que desea agregar a su pedido:\n");
-    listadoArticulos();// Mostrar articulos
-    fechaYHora();
+    listadoArticulos(); // Mostrar articulos
+    fechaYHora();       // Mostrar fecha y hora
     // Pedir id de articulo
 }
 
-// void bajaPedido()
-// {
-//     // Mostrar pedidos del cliente
-//     printf("Id de pedido que desea eliminar: ");
-// }
-//
-// void modificacionPedido()
-// {
-// }
-//
-// void consultaPedido()
-// {
-// }
-//
-// void listadoPedido()
-// {
-// }
+void bajaPedido()
+{
+    // Mostrar pedidos del cliente
+    printf("Id de pedido que desea eliminar: ");
+}
+
+void modificacionPedido() {}
+
+void consultaPedido() {}
+
+void listadoPedido() {}
