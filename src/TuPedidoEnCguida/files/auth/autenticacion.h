30b115732817a6a65eef346677c131be3c71e71b
matearg <mateoarcidiacono@gmail.com>
2022-06-30 16:06:09 -0300

    Login testeado

--- src/TuPedidoEnCguida/files/auth/autenticacion.h
+++ src/TuPedidoEnCguida/files/auth/autenticacion.h
@@ -2,6 +2,9 @@
 #define AUTENTICACION_H_INCLUDED
 
 #include "../clientes/cliente.h"
+#include "../common/commonClientes.h"
+#include "../common/mensajes.h"
+#include "../common/leerTeclado.h"
 
 /* Login del usuario */
 int login(arrClientes *arregloClientes, char archivoClientes[]);
